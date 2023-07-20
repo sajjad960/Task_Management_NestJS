41973297a0d6dc7f24de22daa099cbee784dafe1
Sajjad <sajjadhossainsb01@gmail.com>
2023-07-20 13:24:43 +0600

    explored path delete

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './tasks.model';
+import { Task, TaskStatus } from './tasks.model';
 import { CreateTaskDto } from './dto/create-tasks.dto';
 
 @Controller('tasks')
@@ -10,8 +10,25 @@ export class TasksController {
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
   }
+
+  @Get(":id")
+  getTaskById(@Param("id") id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
   @Post()
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
     return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete(":id")
+  deleteTask(@Param("id") id: string): void{
+    return this.tasksService.deleteTask(id);
+  }
+
+  @Patch(":id/status")
+  
+  updateTaskStatus(@Param("id") id: string , @Body("status") status: TaskStatus){
+    return this.tasksService.updateTaskStatus(id, status);
   }
 }
