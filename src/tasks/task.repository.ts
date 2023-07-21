438f34498431c834998f59c5809c626b409063c2
Sajjad <sajjadhossainsb01@gmail.com>
2023-07-21 13:29:32 +0600

    mysql db connected, repository error solved

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,8 +1,23 @@
-import { Entity, EntityRepository, Repository } from "typeorm";
-import { Task } from "./task.entity";
+import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { Task } from './task.entity';
+import { TaskStatus } from './tasks.model';
+import { CreateTaskDto } from './dto/create-tasks.dto';
 
-@EntityRepository(Task)
+@Injectable()
 export class TaskRepository extends Repository<Task> {
-    
+  constructor(private dataSource: DataSource) {
+    super(Task, dataSource.createEntityManager());
+  }
 
+  async createTask({ title, description }: CreateTaskDto): Promise<Task> {
+    const task = this.create({
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    });
+
+    await this.save(task);
+    return task;
+  }
 }
